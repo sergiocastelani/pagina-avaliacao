30985c4e232943cf53d74957b42793cc00429137
ProgSamuel <progsamu@gmail.com>
2023-10-12 22:06:41 -0300

    Start - Create Card

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
-
+import CreateCards from './Components/card'
+import {Card} from './Style'
 import './App.css'
 
 function App() {
  
 
   return (
-    <>
-      <h1>Hello World!</h1>
-    </>
+    <Card>
+      <CreateCards name='Samu' photo='https://imagedelivery.net/9sCnq8t6WEGNay0RAQNdvQ/cldepgc7t0009ju08myoyr3pu_3/public'></CreateCards>
+    </Card>
   )
 }
 
---
+++ src/Style.tsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Card = styled.div`
+    width: 14em;
+    display: flex;
+    gap: .6em;
+    flex-direction: column;
+    justify-content: center;
+    padding: 1em 2em;
+    border-radius: 1em;
+    background-color: rgba(173, 216, 230, 0.8); 
+    backdrop-filter: blur(10px); 
+    color: #333; 
+`;
