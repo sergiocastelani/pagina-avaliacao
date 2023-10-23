58d3d6a3cf0b41c4f51b1046fbb9866612992e71
Sergio C Castelani Jr <sergio.castelani@gmail.com>
2023-10-23 20:34:30 -0300

    Receita para criar Persons

--- src/App.tsx
+++ src/App.tsx
@@ -2,26 +2,40 @@ import { Board } from './Components/Board'
 import { Card } from './Components/Card'
 import './App.css'
 import {RegisterButton} from './Components/RegisterButton'
+import { PersonDb } from './Db/PersonDb'
+import { Person } from './Model/Person'
+import { Ability } from './Model/Ability'
+
+function init() {
+  const pessoas = PersonDb.getAll();
+
+  if(pessoas.length === 0)
+  {
+    const p1 = new Person('Samu', 'https://imagedelivery.net/9sCnq8t6WEGNay0RAQNdvQ/cldepgc7t0009ju08myoyr3pu_3/public');
+    p1.abilities.push(new Ability('Coding', 10));
+    p1.abilities.push(new Ability('Sleeping', 1000));
+    PersonDb.add(p1);
+  }
+
+  console.log(PersonDb.getAll());
+}
+init();
 
 function App() {
 
-
   return (
-    <>
-      <Board>
-        <Card name='Samu' photo='https://imagedelivery.net/9sCnq8t6WEGNay0RAQNdvQ/cldepgc7t0009ju08myoyr3pu_3/public'></Card>
+    <Board>
+      <Card name='Samu' photo='https://imagedelivery.net/9sCnq8t6WEGNay0RAQNdvQ/cldepgc7t0009ju08myoyr3pu_3/public'></Card>
 
-        <Card name='Samu' photo='https://imagedelivery.net/9sCnq8t6WEGNay0RAQNdvQ/cldepgc7t0009ju08myoyr3pu_3/public'></Card>
+      <Card name='Samu' photo='https://imagedelivery.net/9sCnq8t6WEGNay0RAQNdvQ/cldepgc7t0009ju08myoyr3pu_3/public'></Card>
 
-        <Card name='Samu' photo='https://imagedelivery.net/9sCnq8t6WEGNay0RAQNdvQ/cldepgc7t0009ju08myoyr3pu_3/public'></Card>
+      <Card name='Samu' photo='https://imagedelivery.net/9sCnq8t6WEGNay0RAQNdvQ/cldepgc7t0009ju08myoyr3pu_3/public'></Card>
 
-        <Card name='Samu' photo='https://imagedelivery.net/9sCnq8t6WEGNay0RAQNdvQ/cldepgc7t0009ju08myoyr3pu_3/public'></Card>
+      <Card name='Samu' photo='https://imagedelivery.net/9sCnq8t6WEGNay0RAQNdvQ/cldepgc7t0009ju08myoyr3pu_3/public'></Card>
 
 
-        <RegisterButton></RegisterButton>
-      </Board>
-    </>
-
+      <RegisterButton></RegisterButton>
+    </Board>
   )
 }
 
