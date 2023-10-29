1cf8a642c9f3d3bfe7a137754b328d52d39d347c
Sergio C Castelani Jr <sergio.castelani@gmail.com>
2023-10-29 19:39:54 -0300

    Arquivo sem uso quebrando a build

--- src/index.ts
+++
@@ -1,5 +0,0 @@
-import { Ability } from "./Model/Ability";
-import { Person } from "./Model/Person";
-
-const user1 = new Person ('Samuel', 'https://imagedelivery.net/9sCnq8t6WEGNay0RAQNdvQ/cldepgc7t0009ju08myoyr3pu_3/public')
-
