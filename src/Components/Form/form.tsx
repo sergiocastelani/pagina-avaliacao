f4293ed693aceca8ab9f2eca4ac8792e0e0bc941
Sergio C Castelani Jr <sergio.castelani@gmail.com>
2023-10-29 12:54:49 -0300

    Configurando rotas

--- src/Components/Form/form.tsx
+++ src/Components/Form/form.tsx
@@ -1,5 +1,4 @@
 import { useForm } from "react-hook-form";
-import { Button } from "../Button"
 import { Container, StyleForm } from "./Styles";
 
 export const Form = ()=> {
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,22 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Form } from './Components/Form/form.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+  },
+  {
+    path: "/edit/:userId",
+    element: <Form/>,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+     <RouterProvider router={router} />
   </React.StrictMode>,
 )
